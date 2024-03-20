cdc1979cfd1a442091cf5138ceba4440855b9330
Obada-Badee <obadabadee3@hotmail.com>
2024-03-21 02:59:03 +0300

    Complete State

--- models/state.py
+++ models/state.py
@@ -10,3 +10,12 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete", backref="state")
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Return all the cities associated with the state"""
+            from models import storage
+            city_objects = storage.all(City)
+            return [city for city in city_objects.values()
+                    if city.state_id == self.id]
