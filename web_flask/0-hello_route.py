9f774523a5486b9179df5cf80c556437a158fea0
Obada-Badee <obadabadee3@hotmail.com>
2024-04-27 18:34:53 +0300

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""A script to start a flask web application"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """Home route"""
+    return "Hello HBNB!"
+
+
+if (__name__ == "__main__"):
+    app.run(host="0.0.0.0", port=5000, debug=True)
